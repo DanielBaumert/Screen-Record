e0a9e2c80871fb0aa25e34da006d920120044eba
Daniel Baumert <DBaumertffg@web.de>
2018-12-13 08:02:16 +0100

    Update RECT.cs

--- Structers/RECT.cs
+++ Structers/RECT.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 
-namespace ToolKitRAT.WinApi.Structers {
+namespace ToolKit.WinApi.Structers {
     [StructLayout(LayoutKind.Sequential)]
     public struct RECT {
         public int Left { get; private set; }
